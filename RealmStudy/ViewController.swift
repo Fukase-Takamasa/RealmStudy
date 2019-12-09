81d2076d4eccd638235d7776143d590024acf00c
深瀬 貴将 <fukase@arsaga.jp>
2019-12-09 16:43:07 +0900

    Initial Commit

---
+++ RealmStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmStudy
+//
+//  Created by 深瀬 貴将 on 2019/12/09.
+//  Copyright © 2019 深瀬 貴将. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
