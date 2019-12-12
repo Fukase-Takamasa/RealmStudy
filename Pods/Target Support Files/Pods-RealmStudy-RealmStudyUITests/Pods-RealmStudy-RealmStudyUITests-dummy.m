dfe6c794a468fa3199e0e483467801a2da01e59f
深瀬 貴将 <fukase@arsaga.jp>
2019-12-12 11:19:22 +0900

    fix .gitignore

--- Pods/Target Support Files/Pods-RealmStudy-RealmStudyUITests/Pods-RealmStudy-RealmStudyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RealmStudy_RealmStudyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_RealmStudy_RealmStudyUITests
-@end
--- Pods/Target Support Files/Pods-RealmStudyTests/Pods-RealmStudyTests-dummy 3.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RealmStudyTests : NSObject
-@end
-@implementation PodsDummy_Pods_RealmStudyTests
-@end
