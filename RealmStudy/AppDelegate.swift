47b078b48225d0ae24300bc9b061f6fd6104c2f2
深瀬 貴将 <fukase@arsaga.jp>
2019-12-18 11:26:44 +0900

    Realmマイグレーションの記述をAppDelegateに移動した。

--- RealmStudy/AppDelegate.swift
+++ RealmStudy/AppDelegate.swift
@@ -15,16 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //let config = Realm.Configuration(
-          //  schemaVersion: 3,
-            //migrationBlock: { migration, oldScheamVersion in
-              //  if (oldScheamVersion < 3) {
-///
-  //              }
-    //    })
-//aaaa
-        //bbbb
-    //    Realm.Configuration.defaultConfiguration = config
+        let config = Realm.Configuration(
+            schemaVersion: 3,
+            migrationBlock: { migration, oldScheamVersion in
+               if (oldScheamVersion < 3) {
+
+               }
+        })
+
+        Realm.Configuration.defaultConfiguration = config
         return true
         
     }
